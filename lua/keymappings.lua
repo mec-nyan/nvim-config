344122f8d22ee0478f2eb7edbae5fe9ee6a1c166
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-08-07 23:57:40 -0300

    Trying new description style.

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -7,21 +7,21 @@
 local setkey = vim.keymap.set
 
 -- Write
-setkey('n', '<M-w>', '<cmd>write<cr>', { desc = 'write (normal)' })
-setkey('i', '<M-w>', '<esc><cmd>write<cr>', { desc = 'write (insert)' })
+setkey('n', '<M-w>', '<cmd>write<cr>', { desc = '[n] write' })
+setkey('i', '<M-w>', '<esc><cmd>write<cr>', { desc = '[i] write' })
 
 -- Quit
-setkey('n', '<M-q>', '<cmd>quit<cr>', { desc = 'quit (normal)' })
-setkey('i', '<M-q>', '<esc><cmd>quit<cr>', { desc = 'quit (normal)' })
+setkey('n', '<M-q>', '<cmd>quit<cr>', { desc = '[n] quit' })
+setkey('i', '<M-q>', '<esc><cmd>quit<cr>', { desc = '[i] quit' })
 
 -- Windows, tabs
-setkey('n', '<M-o>', '<cmd>only<cr>', { desc = 'only' })
+setkey('n', '<M-o>', '<cmd>only<cr>', { desc = '[n] only' })
 
 -- Movement
-setkey('n', 'ge', 'G', { desc = 'end of file' })
+setkey('n', 'ge', 'G', { desc = '[move] end of file' })
 
 -- Search
-setkey('n', '<leader>l', '<cmd>nohlsearch<cr>', { desc = 'clear searh highlight' })
+setkey('n', '<leader>l', '<cmd>nohlsearch<cr>', { desc = '[n] clear searh highlight' })
 
 
 return {}
