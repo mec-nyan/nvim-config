3d4534b1cdc9a42c12ce78502bb1290a26dfe02d
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-08-10 18:53:52 -0300

    Added: command line autocompletion.

--- lua/extras.lua
+++ lua/extras.lua
@@ -6,8 +6,26 @@
 --     Command line autocompletion.
 --     Live grep.
 --
+--
+--  I'll put these here and then move them to their own files if needed.
+--
 --]]
 
 
+-- See `:h cmdline-autocompletion` for details.
+local function set_cmdline_autocompletion()
+	vim.api.nvim_create_autocmd('CmdlineChanged', {
+		pattern = { ':', '/', '?' },
+		callback = function()
+			vim.fn.wildtrigger()
+		end,
+	})
+
+	vim.o.wildmode = 'noselect:lastused,full'
+	vim.o.wildoptions = 'pum'
+end
+
+
+set_cmdline_autocompletion()
 
 return {}
