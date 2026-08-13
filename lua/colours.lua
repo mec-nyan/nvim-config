839c6567a9a7f9057b06c6f9dd4210fb9afb61a7
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-08-13 18:41:34 -0300

    [colours] Darker background for preview window.

--- lua/colours.lua
+++ lua/colours.lua
@@ -9,4 +9,14 @@ vim.cmd[[colorscheme catppuccin]]
 
 vim.cmd.highlight{'Normal', 'guibg=None'}
 
+-- Use a darker background for other windows i.e. quickfix, preview, etc.
+
+vim.api.nvim_create_autocmd('WinEnter', {
+	callback = function()
+		if vim.wo.previewwindow then
+			vim.wo.winhighlight = 'Normal:Pmenu'
+		end
+	end
+})
+
 return {}
