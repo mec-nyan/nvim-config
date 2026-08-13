3836b85a1ae3fc977318735c8536693ff3797236
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-08-13 18:48:37 -0300

    [colours] Darker bg for qflist and loclist windows.

--- lua/colours.lua
+++ lua/colours.lua
@@ -11,11 +11,20 @@ vim.cmd.highlight{'Normal', 'guibg=None'}
 
 -- Use a darker background for other windows i.e. quickfix, preview, etc.
 
+-- Preview is a special window:
 vim.api.nvim_create_autocmd('WinEnter', {
 	callback = function()
 		if vim.wo.previewwindow then
 			vim.wo.winhighlight = 'Normal:Pmenu'
 		end
+	end
+})
+
+-- Both loclist and qflist have the same filetype `qf`.
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'qf' },
+	callback = function()
+		vim.wo.winhighlight = 'Normal:Pmenu'
 	end
 })
 
