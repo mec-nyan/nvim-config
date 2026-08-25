c3232d711c1c9b07a8b12a62540977530269c592
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-08-25 18:40:36 -0300

    [diff] status line option for diff windows.

--- lua/diff.lua
+++ lua/diff.lua
@@ -5,3 +5,14 @@
 --
 --
 --]]
+
+-- Show separate status lines for diff windows.
+vim.api.nvim_create_autocmd('WinEnter', {
+	callback = function()
+		if vim.wo.diff then
+			vim.opt.laststatus = 2
+		else
+			vim.opt.laststatus = 3
+		end
+	end,
+})
